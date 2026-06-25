444c902bb47dcb0933493189510ff68b469636e1
carlosherre <acmontero@uninorte.edu.co>
2026-06-24 23:26:36 -0500

    Solución de cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 // CORS
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
